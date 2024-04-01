5f7438ed061f45acac8bade823ba04f9bb749484
KaliuzhnaMariia <kaliuzhna.mariia@chnu.edu.ua>
2024-04-01 12:26:09 +0300

    Task 1 done (final version)

--- Lab7CSharp/Form1.Designer.cs
+++ Lab7CSharp/Form1.Designer.cs
@@ -30,7 +30,9 @@ namespace Lab7CSharp
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Dupbutton = new System.Windows.Forms.Button();
+            this.Exbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -47,21 +49,42 @@ namespace Lab7CSharp
             this.label1.Text = "Lab 7.   C# ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.Location = new System.Drawing.Point(166, 103);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.textBox1.Location = new System.Drawing.Point(54, 45);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 195);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Dupbutton
+            // 
+            this.Dupbutton.Location = new System.Drawing.Point(84, 303);
+            this.Dupbutton.Name = "Dupbutton";
+            this.Dupbutton.Size = new System.Drawing.Size(93, 32);
+            this.Dupbutton.TabIndex = 2;
+            this.Dupbutton.Text = "Duplicate";
+            this.Dupbutton.UseVisualStyleBackColor = true;
+            this.Dupbutton.Click += new System.EventHandler(this.Dupbutton_Click);
+            // 
+            // Exbutton
+            // 
+            this.Exbutton.Location = new System.Drawing.Point(233, 303);
+            this.Exbutton.Name = "Exbutton";
+            this.Exbutton.Size = new System.Drawing.Size(99, 32);
+            this.Exbutton.TabIndex = 3;
+            this.Exbutton.Text = "Exit";
+            this.Exbutton.UseVisualStyleBackColor = true;
+            this.Exbutton.Click += new System.EventHandler(this.Exbutton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 386);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Exbutton);
+            this.Controls.Add(this.Dupbutton);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
@@ -74,7 +97,9 @@ namespace Lab7CSharp
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Dupbutton;
+        private System.Windows.Forms.Button Exbutton;
     }
 }
 
